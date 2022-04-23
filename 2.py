7c56da66d9d189b4687a398a4850b838984fc334
hanumabhavani <103923527+hanumabhavani@users.noreply.github.com>
2022-04-23 15:34:08 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,8 @@
+#finding maximum number between two numbers
+
+num1 = int(input('enter number1: '))
+num2 = int(input('enter number2: '))
+if num1 > num2:
+    print('number 1 is maximum')
+else:
+    print('number 2 is maximum')
